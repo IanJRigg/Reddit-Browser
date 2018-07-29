26baa568d8f2ea78a4efd2905f4060749de04d7b
Ian <ianjrigg@gmail.com>
2018-07-29 13:10:33 -0700

    Write functionality is there. Need to test

--- parser/parser.py
+++ parser/parser.py
@@ -5,6 +5,15 @@ import sqlite3
 from sqlite3 import Error
 
 
+# SQL Statements
+create_table_command = """ CREATE TABLE IF NOT EXISTS posts (
+                               ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                               post TEXT NOT NULL
+                           ); """
+
+insert_post_command = """ INSERT INTO posts(post) VALUES(?);"""
+
+
 
 def parse_config_file():
     config = configparser.ConfigParser()
@@ -12,9 +21,26 @@ def parse_config_file():
     return config
 
 
+def create_tables(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(create_table_command)
+    except Error as error:
+        print(error)
+
+def insert_post(conn, comment):
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(insert_post_command, (comment.body, ));
+    except Error as error:
+        print(error)
+
+
 def create_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
+        create_tables(conn)
         return conn
     except Error as error:
         print(error)
@@ -38,17 +64,17 @@ def search_reddit():
 
     conn = create_connection(config["PARAMETERS"]["SQLiteFile"])
 
-    for submission in subreddit.hot(limit=10):
+    for submission in subreddit.hot(limit=25):
 
+        # Remove the more commments comment (requires another API hit)
         submission.comments.replace_more(limit=0)
 
         for comment in submission.comments:
             text = comment.body.lower()
 
             if term in text:
-                print(text)
+                insert_post(conn, comment)
 
-    print(count)
 
 if __name__ == '__main__':
     search_reddit()
